18878e2f8076dd3462b7e2e2522400cd70c06c34
danielroufe <daniel.roufe@gmail.com>
2025-10-05 18:59:34 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='kidroutine-son-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
